2020ed5f3b16cacac1f9dceb76c4cb44b237992c
unknown <anr66@zips.uakron.edu>
2019-03-23 01:30:37 -0400

    Add project

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public static int score_counter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        string my_material_name = GetComponent<Renderer>().material.name;
+        var color_only = my_material_name.Substring(0, 1);
+
+        Debug.Log(color_only);
+
+        if (color_only == "r")
+        {
+            score_counter += 1;
+            Destroy(transform.root.gameObject);
+        }
+
+        else if (color_only == "g")
+        {
+            score_counter += 2;
+            Destroy(transform.root.gameObject);
+        }
+
+        else if (color_only == "b")
+        {
+            score_counter += 3;
+            Destroy(transform.root.gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/MakeBalls.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeBalls : MonoBehaviour
+{
+    public GameObject blue_ball;
+    public GameObject red_ball;
+    public GameObject green_ball;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int r = 0; r < 100; r++)
+        {
+            Instantiate(red_ball, new Vector3(Random.Range(-21f, 26f), 1, Random.Range(2f, 50f)), Quaternion.identity);
+        }
+
+        for (int g = 0; g < 50; g++)
+        {
+            Instantiate(green_ball, new Vector3(Random.Range(-21f, 26f), 1, Random.Range(2f, 50f)), Quaternion.identity);
+        }
+
+        for (int b = 0; b < 25; b++)
+        {
+            Instantiate(blue_ball, new Vector3(Random.Range(-21f, 26f), 1, Random.Range(2f, 50f)), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+}
